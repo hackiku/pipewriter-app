ed7390166af7cde13354d7d401fbc54cb1530bd8
hackiku <ivan@pipewriter.io>
2024-07-25 13:41:33 +0200

    shadcn, lucide, dark mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  import { toggleMode } from "mode-watcher";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { Badge } from "$lib/components/ui/badge";
+
+  import { Sun, Moon } from "lucide-svelte";
+</script>
+
+<div class="flex items-center">
+  <h1 class="">Welcome to SvelteKit</h1>
+  <Badge variant="outline">Badge</Badge>
+</div>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button on:click={toggleMode} variant="outline" size="icon">
+  <Sun
+    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+  />
+  <Moon
+    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+  />
+  <span class="sr-only">Toggle theme</span>
+</Button>
