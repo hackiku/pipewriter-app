57c84180a87bcfb906f947505f0f0e5dd47f5910
hackiku <ivan@pipewriter.io>
2024-09-11 11:07:13 +0200

    layout app.css /app

---
+++ src/routes/(app)/app/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../../../app.css";
+	import AppHeader from "$lib/components/AppHeader.svelte";
+	// import Nav from "$lib/components/Nav.svelte";
+
+  import { ModeWatcher } from "mode-watcher";
+</script>
+
+<ModeWatcher />
+<!-- <Nav /> -->
+
+<!-- <AppHeader /> -->
+<slot />
