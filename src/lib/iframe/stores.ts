f9582babda25ae35bf94fd0409c8d1882e0115fa
hackiku <ivan@pipewriter.io>
2024-10-23 15:56:36 +0200

    deploying more polished

--- src/lib/iframe/stores.ts
+++ src/lib/iframe/stores.ts
@@ -2,8 +2,8 @@
 
 import { writable } from "svelte/store";
 
-export const showInfo = writable(false);
+export const showInfo = writable(false); // show
 export const chainMode = writable(false);
 export const showLabels = writable(false);
 export const activeTab = writable(false);
-export const elementsTheme = writable(false);
+export const elementsTheme = writable<"light" | "gray" | "dark">("light");
