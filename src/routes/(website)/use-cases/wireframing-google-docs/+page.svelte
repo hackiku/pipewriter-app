e244e20456632c71d6af431e9a1d4db31c27ebab
hackiku <ivan@pipewriter.io>
2025-02-16 00:06:32 +0100

    seo: test /use-cases route & sample seo page

---
+++ src/routes/(website)/use-cases/wireframing-google-docs/+page.svelte
@@ -0,0 +1,6 @@
+<!-- src/routes/(website)/use-cases/wireframing-google-docs/+page.svelte -->
+<script>
+  import WireframingGoogleDocs from "$lib/pages/use-cases/WireframingGoogleDocs.svelte";
+</script>
+
+<WireframingGoogleDocs />
